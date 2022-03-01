25cbc04415287b6e026726181f6b43db3e502ca9
talcohen2021 <talcohen@g.jct.ac.il>
2022-03-01 15:40:36 +0200

    # WARNING: head commit changed in the meantime
    changes to point

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,11 +1,30 @@
 package primitives;
 //to do - function add/subtract, constructor, documentation, 
-//questions - returning a vector? use of final - must make a new point/double3 when making changes?
+//questions - still need distance and distance squared?
 
 
 public class Point {
+	
 	final Double3 xyz;
 
+	/** Constructor to initialize Point based object with its Double3
+	 * 
+	 * @param double3 coordinate values
+	 */
+	protected Point(Double3 double3) {
+		this.xyz = double3;
+	}
+	/**
+	 * Constructor to initialize Point based object with its three number values
+	 * 
+	 * @param d1 first number value
+	 * @param d2 second number value
+	 * @param d3 third number value
+	 */
+	protected Point(double d1, double d2, double d3) {
+		this.xyz(d1, d2, d3);
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -18,17 +37,19 @@ public class Point {
 	
 	
 	@Override
-	public String toString() {return xyz.toString();}
+	public String toString() {
+		return xyz.toString();	
+	}
 	
 	//@Override
 	public Point add(Vector vector) {
-		return null;
+		return new Point(this.xyz.add(vector.xyz));
 		//to do
 	}
 	
 	//@Override
 	public Vector subtract(Point point) {
-		return null;
+		return new Vector(this.xyz.subtract(vector.xyz));
 		//to do
 	}
 }
