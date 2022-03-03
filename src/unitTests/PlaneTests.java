9d600e7e1478573c45eba31df721506ebfd07bcf
Yaakovah <Yaakovah@g.jct.ac.il>
2022-03-03 20:09:18 +0200

    added some testing packages

---
+++ src/unitTests/PlaneTests.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Yaakovah
+ *
+ */
+class PlaneTests {
+
+	/**
+	 * Test method for {@link geometries.Plane#getNormal(java.awt.Point)}.
+	 */
+	@Test
+	void testGetNormalPoint() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link geometries.Plane#getNormal(java.awt.Point, java.awt.Point, java.awt.Point)}.
+	 */
+	@Test
+	void testGetNormalPointPointPoint() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/unitTests/PointTests.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Yaakovah
+ *
+ */
+class PointTests {
+
+	/**
+	 * Test method for {@link primitives.Point#add(primitives.Vector)}.
+	 */
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link primitives.Point#subtract(primitives.Point)}.
+	 */
+	@Test
+	void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link primitives.Point#distanceSquared(primitives.Point)}.
+	 */
+	@Test
+	void testDistanceSquared() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link primitives.Point#distance(primitives.Point)}.
+	 */
+	@Test
+	void testDistance() {
+		fail("Not yet implemented");
+	}
+
+}
