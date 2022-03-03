9bb5c2ebf3dc4733eee0eb618ff21a6c11bc7098
meira grafstein <grafstei@g.jct.ac.il>
2022-03-03 20:13:18 +0200

    added sphere test

---
+++ src/unitTests/SphereTests.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author meira
+ *
+ */
+class SphereTests {
+
+	/**
+	 * Test method for {@link geometries.Sphere#getNormal(Point)}.
+	 */
+	@Test
+	void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+}
