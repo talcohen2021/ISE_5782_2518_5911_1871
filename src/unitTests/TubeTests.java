55726b20c036aec048e0232fe6cc0bf73e33d97e
talcohen2021 <talcohen@g.jct.ac.il>
2022-03-03 20:16:08 +0200

    added Tube test

---
+++ src/unitTests/TubeTests.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Merekat
+ *
+ */
+class TubeTests {
+
+	/**
+	 * Test method for {@link geometries.Tube#getNormal(Point)}.
+	 */
+	@Test
+	void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+}
