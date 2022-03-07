0328091f2e12fffb24fec9988e909fb560f6a920
talcohen2021 <talcohen@g.jct.ac.il>
2022-03-07 22:34:04 +0200

    Sphere get normal
    untested and commented out as sphereTests not done yet

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -1,12 +1,22 @@
 package geometries;
 
+import primitives.Point;
+import primitives.Vector;
+
 public class Sphere implements Geometry {
-	final Point center;
+	final Point centre;
 	final double radius;
 	
 	@Override
 	public Vector getNormal(Point point) {
 		return null;
+		/* Maybe?
+		 * 
+		 * Vector v = new Vector(point.xyz.subtract(centre));
+		 * Vector u = v.normalize();
+		 * return u;
+		 * 
+		 */
 	}
 
 }
