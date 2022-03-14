26b964dd96943d9f74e8d602b186e2b28027066c
Yaakovah <Yaakovah@g.jct.ac.il>
2022-03-14 11:44:37 +0200

    finished tube and tube tests

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -6,10 +6,19 @@ import primitives.Vector;
 
 public class Tube implements Geometry{
 	
-	final Ray axisRay = null;
-	final double radius = 0;
+	final Ray axisRay;
+	final double radius;
 	
-	
+	/** Constructor to initialize Tube object with a Ray and a double
+	 * 
+	 * @param axisRay - Ray for the tube
+	 * @param radius - radius of the tube
+	 */
+	public Tube(Ray axisRay, double radius) {
+		this.axisRay = axisRay;
+		this.radius = radius;
+	}
+
 	public Ray getAxisRay()
 	{
 		return axisRay;
@@ -20,8 +29,17 @@ public class Tube implements Geometry{
 		return radius;
 	}
 	
-	public Vector getNormal(Point point) {
-		return null;
+	/** method to get the nomal of the tube from a given point that lies on the tube
+	 * 
+	 * @param point - point that lays on the body of the tube, where the normal will be from
+	 * @throws Exception 
+	 */
+	public Vector getNormal(Point point) throws Exception {
+		// o = projection of Point on cylinder's ray = P0 + v.scale(t)
+		//t = distance = v.dotProduct(P-P0)
+		double t = this.axisRay.getDir().dotProduct(this.axisRay.getP0().subtract(point));
+		Point o = this.axisRay.getP0().add(this.axisRay.getDir().scale(t));
+		return point.subtract(o);
 	}
 
 }
--- src/unitTests/TubeTests.java
+++ src/unitTests/TubeTests.java
@@ -7,27 +7,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import geometries.Polygon;
+import geometries.Tube;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
 /**
- * @author meira
+ * @author Yaakovah
  *
  */
 class TubeTests {
+	
+	/**
+	 * Test method for {@link geometries.Tube#Tube(...)}.
+	 */
+	@Test
+	public void testConstructor() throws Exception {
+	
+		// =============== Boundary Values Tests ==================
+		//radius: does not equal 0
+
+
+		Tube t = new Tube(new Ray(new Point(0,0,0), new Vector(1,1,1)), 0);
+		assertNotEquals(t.getRadius(), 0, "bad radius for tube");
+	}
 
 	/**
 	 * Test method for {@link geometries.Tube#getNormal(Point)}.
+	 * @throws Exception 
 	 */
 	@Test
-	void testGetNormal() {
+	void testGetNormal() throws Exception {
 		fail("Not yet implemented");
 		
-		Tube t = new Tube();
-		
-		
-/*
- * •	In the tube tests there is one equivalent partition and one boundary test 
- * (when the connection between the point on the body and the ray’s head creates a 90 degrees with
- *  the ray).
- * */
+		// ============ Equivalence Partitions Tests ==============
+		// connection between the point on the body and the ray’s head creates a 90 degrees with the ray
+
+		Tube t = new Tube(new Ray(new Point(0,0,0), new Vector(1,1,1)), 1);
+		double lengthV = t.getAxisRay().getDir().length();
+		double lengthNormal = t.getNormal(new Point(0,0,1)).length();
+		assertEquals(lengthV*lengthNormal, t.getAxisRay().getDir().crossProduct(new Point(0,0,1).subtract(t.getAxisRay().getP0())), "Bad normal to tube");
+	
 	}
 
 }
+
