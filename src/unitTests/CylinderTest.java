8e79ce29f19fcbd4f422fe335646c9d1bb4bd611
meira grafstein <grafstei@g.jct.ac.il>
2022-03-14 19:53:47 +0200

    Add cylinderTest

---
+++ src/unitTests/CylinderTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author meira
+ *
+ */
+class CylinderTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/unitTests/TriangleTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author meira
+ *
+ */
+class TriangleTest {
+
+	/**
+	 * Test method for {@link geometries.Polygon#getNormal(primitives.Point)}.
+	 */
+	@Test
+	void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+}
