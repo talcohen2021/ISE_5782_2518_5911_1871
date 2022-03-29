9d08d144ca3458bb4dcd0efd6a3afaf501edb9f0
meira grafstein <grafstei@g.jct.ac.il>
2022-03-29 21:55:27 +0300

    did sphere findIntersections

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -15,6 +15,7 @@ public interface Intersectable {
 	 * 
 	 * @param ray - a ray that may or may not intersect with the geometry. 
 	 * @return - a list of all of the points of intersection or null (in the case that there is no intersection points)
+	 * @throws Exception 
 	 */
-	List<Point> findIntsersections(Ray ray);
+	List<Point> findIntsersections(Ray ray) throws Exception;
 }
--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -1,5 +1,6 @@
 package geometries;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import primitives.Point;
@@ -33,10 +34,36 @@ public class Sphere implements Geometry {
 		return this.radius;
 	}
 
+	/**
+	 * @brief this function finds the intersections between a ray and a sphere
+	 * per the slides from last semester
+	 * @param the ray that is intersecting the sphere 
+	 * @returns the list of intersections bw the ray and sphere if there are any
+	 */
 	@Override
-	public List<Point> findIntsersections(Ray ray) {
-		Vector u = new Vector()
-		return null;
+	public List<Point> findIntsersections(Ray ray) throws Exception {
+		
+		//u = O - P0
+		Vector u = centre.subtract(ray.getP0());
+		//tm = v*u
+		double tm = ray.getDir().dotProduct(u);
+		double dSquared = u.lengthSquared() - tm*tm;
+		double d = Math.sqrt(dSquared);
+		//if d>=r no intersections
+		if (d >= radius)
+			return null;
+		double th = Math.sqrt(radius*radius - dSquared);
+		double t1 = tm + th;
+		double t2= tm - th;
+		
+		List<Point> results = new LinkedList<Point>();
+		
+		if(t1 > 0)
+			results.add(ray.getP0().add(ray.getDir().scale(t1)));
+		if(t2 > 0)
+			results.add(ray.getP0().add(ray.getDir().scale(t2)));
+		return results;
+		
 	}
 
 }
