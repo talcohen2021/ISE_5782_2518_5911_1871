13c13d23dd4cdd3085f2c1c1893a748c2186c7b9
meira grafstein <grafstei@g.jct.ac.il>
2022-06-07 18:18:57 +0300

    added picturetest class

---
+++ src/unitTests/renderer/PictureTests.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package unitTests.renderer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author meira
+ *
+ */
+class PictureTests {
+
+	/**
+	 * 
+	 */
+	@Test
+	public void makePicture() {
+		fail("Not yet implemented");
+	}
+
+}
