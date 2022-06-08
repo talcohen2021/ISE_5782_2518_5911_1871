12f0d7e6be1a6084b3fe281b1d634cd669b11e42
talcohen2021 <talcohen@g.jct.ac.il>
2022-06-08 15:13:11 +0300

    picture stage 7 finalised

--- src/unitTests/renderer/PictureTests.java
+++ src/unitTests/renderer/PictureTests.java
@@ -60,11 +60,11 @@ public class PictureTests {
 	private Material material2 = new Material().setKD(0.9).setKS(0.9).setShininess(500);
 	
 	private Geometry triangle1 = new Triangle(p[0], p[4], p[8]).setMaterial(material); //a,e,i
-	private Geometry triangle2 = new Triangle(p[2], p[4], p[8]).setMaterial(material2); //c, e, i
+	//private Geometry triangle2 = new Triangle(p[2], p[4], p[8]).setMaterial(material2); //c, e, i
 	private Geometry triangle3 = new Triangle(p[0], p[4], p[7]).setMaterial(material); //a, e, h
 	private Geometry triangle4 = new Triangle(p[3], p[4], p[7]).setMaterial(material2); //d, e, h
 	private Geometry triangle5 = new Triangle(p[1], p[4], p[5]).setMaterial(material); //b, e, f
-	private Geometry triangle6 = new Triangle(p[3], p[4], p[5]).setMaterial(material2); //d, e, f
+	//private Geometry triangle6 = new Triangle(p[3], p[4], p[5]).setMaterial(material2); //d, e, f
 	private Geometry triangle7 = new Triangle(p[1], p[4], p[6]).setMaterial(material); //b, e, g
 	private Geometry triangle8 = new Triangle(p[2], p[4], p[6]).setMaterial(material2); //c, e, g
 	
@@ -79,35 +79,32 @@ public class PictureTests {
 	
 		scene.getGeometries().add(
 				triangle1.setEmission(new Color(WHITE)).setEmission(new Color(BLUE)) 
-				.setMaterial(new Material().setKR(0.5).setKS(0.5).setKT(0.2)), 
-				//triangle2.setEmission(new Color(RED)), 
+					.setMaterial(new Material().setKR(0.5).setKS(0.5).setKT(0.2)), 
 				triangle3.setEmission(new Color(WHITE)).setEmission(new Color(BLUE)) 
-				.setMaterial(new Material().setKR(0.5).setKS(0.5).setShininess(30)), 
-				//triangle4.setEmission(new Color(YELLOW)),
+					.setMaterial(new Material().setKR(0.5).setKS(0.5).setShininess(30)), 
 				triangle5.setEmission(new Color(WHITE)).setEmission(new Color(BLUE)), 
 				//.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(30)),
-				//triangle6.setEmission(new Color(WHITE)),
 				triangle7.setEmission(new Color(WHITE)).setEmission(new Color(BLUE)),
-				//triangle8.setEmission(new Color(PINK))
 				//make shpere on top left
 				new Sphere(new Point(-60, 50, -50), 30d).setEmission(new Color(BLUE)) 
-				.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(30).setKT(new Double3(0.8))),
+					.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(30).setKT(new Double3(0.8))),
 				//make 2 little spheres inside the above sphere
-				new Sphere(new Point(-55, 50, -50), 10d).setEmission(new Color(BLACK)) 
-				.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(30)),
-				new Sphere(new Point(-65, 50, -50), 10d).setEmission(new Color(BLACK)) 
-				.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(30)),
+				new Sphere(new Point(-55, 55, -50), 10d).setEmission(new Color(BLACK)) //inner circle higher
+					.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(15).setKR(new Double3(0.5))),
+				new Sphere(new Point(-65, 45, -25), 7.5d).setEmission(new Color(BLACK)) //inner circle lower
+					.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(30).setKR(new Double3(0.5))),
+				new Sphere(new Point(-55, 45, -35), 7.5d).setEmission(new Color(BLACK)) //inner circle lower right
+					.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(25).setKR(new Double3(0.5))),
 				//make sphere on bottom right
 				new Sphere(new Point(60, -50, -50), 30d).setEmission(new Color(BLUE)) 
-				.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(30)));
+					.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(30)));
 	
 		scene.getLights().add(new DirectionalLight(new Color(RED), new Vector(-2,-2,-2)));
 		scene.getLights().add(new DirectionalLight(new Color(GREEN), new Vector(0,1,-50)));
 		//create shadows of the spheres
 		scene.getLights().add(new SpotLight(new Color(700, 400, 400), new Point(60, -50, 0),
 				new Vector(0, 0, -1)).setKL(4E-5).setKQ(2E-7)); 
-		//scene.getLights().add(new SpotLight(new Color(700, 400, 400), new Point(-60, 50, 0),
-			//	new Vector(0, 0, -1)).setKL(4E-5).setKQ(2E-7)); 
+
 		
 		ImageWriter imageWriter = new ImageWriter("ourPicture", 500, 500);
 		camera.setImageWriter(imageWriter) //
@@ -123,13 +120,9 @@ public class PictureTests {
 	@Test
 	public void makePicture2() throws Exception {
 	
-		scene.getGeometries().add(triangle1.setEmission(new Color(BLUE)), 
-				//triangle2.setEmission(new Color(RED)), 
-				//triangle3.setEmission(new Color(GREEN)), 
+		scene.getGeometries().add(triangle1.setEmission(new Color(BLUE)),  
 				triangle4.setEmission(new Color(YELLOW)),
 				triangle5.setEmission(new Color(BLACK)),
-				//triangle6.setEmission(new Color(WHITE)),
-				//triangle7.setEmission(new Color(ORANGE)),
 				triangle8.setEmission(new Color(PINK)),
 				new Sphere(new Point(0, 0, -50), 25d).setEmission(new Color(RED)) 
 						.setMaterial(new Material().setKD(0.4).setKS(0.3).setShininess(100).setKT(new Double3(0.3))),
@@ -144,52 +137,9 @@ public class PictureTests {
 		camera.renderImage();
 		camera.writeToImage(); 		
 	}
-	/* copy of method so that if we make changes we have what to look back on
-	 * public void makePicture2() throws Exception {
-	
-		scene.getGeometries().add(triangle1.setEmission(new Color(BLUE)), 
-				//triangle2.setEmission(new Color(RED)), 
-				//triangle3.setEmission(new Color(GREEN)), 
-				triangle4.setEmission(new Color(YELLOW)),
-				triangle5.setEmission(new Color(BLACK)),
-				//triangle6.setEmission(new Color(WHITE)),
-				//triangle7.setEmission(new Color(ORANGE)),
-				triangle8.setEmission(new Color(PINK)),
-				new Sphere(new Point(0, 0, -50), 25d).setEmission(new Color(RED)) 
-						.setMaterial(new Material().setKD(0.4).setKS(0.3).setShininess(100).setKT(new Double3(0.3))),
-				new Sphere(new Point(0, 0, -50), 12.5d).setEmission(new Color(GREEN)) 
-						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100)));
-	
-		scene.getLights().add(new DirectionalLight(trCL, trDL));
-
-		ImageWriter imageWriter = new ImageWriter("ourPicture2", 500, 500);
-		camera.setImageWriter(imageWriter) //
-				.setRayTraceBase(new RayTracerBasic(scene));
-		camera.renderImage();
-		camera.writeToImage(); 		
-	}
-
-	 * */
 
 }
-/*
- * Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
-				.setVPSize(150, 150).setVPDistance(1000);
 
-		scene.getGeometries().add( //
-				new Sphere(new Point(0, 0, -50), 50d).setEmission(new Color(BLUE)) //
-						.setMaterial(new Material().setKD(0.4).setKS(0.3).setShininess(100).setKT(new Double3(0.3))),
-				new Sphere(new Point(0, 0, -50), 25d).setEmission(new Color(RED)) //
-						.setMaterial(new Material().setKD(0.5).setKS(0.5).setShininess(100)));
-		scene.getLights().add( //
-				new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2)) //
-						.setKL(0.0004).setKQ(0.0000006));
-
-		camera.setImageWriter(new ImageWriter("refractionTwoSpheres", 500, 500)) //
-				.setRayTraceBase(new RayTracerBasic(scene)); //
-		camera.renderImage(); //
-		camera.writeToImage();
- * */
 
 
 
